fedd6756aa138a2d5861389a563e105e5cc8474b
Filipe Sobral <filipe.nsv@gmail.com>
2023-09-26 10:39:37 -0300

    Banco de dados - Banco e Tabelas

---
+++ data base/db_market.sql
@@ -0,0 +1,13 @@
+-- Database: db_market
+
+-- DROP DATABASE IF EXISTS db_market;
+
+CREATE DATABASE db_market
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
---
+++ data base/db_market.tables.sql
@@ -0,0 +1,51 @@
+-- Criação da tabela 'product_type'
+CREATE TABLE product_type (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    tax DECIMAL(8, 3) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Criação da tabela 'product'
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    product_type_id INTEGER REFERENCES product_type(id),
+    value DECIMAL(8, 3) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Criação da tabela 'transactions'
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    transaction_type VARCHAR(255) NOT NULL,
+    supplier_name VARCHAR(255),
+    customer_name VARCHAR(255),
+    product_id INTEGER REFERENCES product(id),
+    amount INTEGER NOT NULL,
+    value_without_tax DECIMAL(8, 3),
+    total_tax DECIMAL(8, 3),
+    total_value DECIMAL(8, 3) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Criação da tabela 'stock'
+CREATE TABLE stock (
+    id SERIAL PRIMARY KEY,
+    transaction_id INTEGER REFERENCES transactions(id),
+    product_id INTEGER REFERENCES product(id),
+    value_without_tax DECIMAL(8, 3),
+    total_tax DECIMAL(8, 3),
+    total_value DECIMAL(8, 3),
+    status VARCHAR(255),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
